1f257508ded9ea18243e7958ec3e5d4d1dfa3f8a
Dcoder10M <84701974+Dcoder10M@users.noreply.github.com>
2023-06-11 18:27:40 +0530

    Time: 293 ms (89.48%), Space: 94.2 MB (76.82%) - LeetHub

--- 1661-minimum-number-of-vertices-to-reach-all-nodes/1661-minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1661-minimum-number-of-vertices-to-reach-all-nodes/1661-minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
-    set<int> ans;
-    void dfs(int node,vector<vector<int>> &adj,vector<int> &vis){
-        vis[node]=1;
-        for(auto i:adj[node]){
-            if(!vis[i]){
-                dfs(i,adj,vis);
-            }else{
-                if(ans.find(i)!=ans.end())ans.erase(i);
-            }
-        }
-    }
+    // set<int> ans;
+    // void dfs(int node,vector<vector<int>> &adj,vector<int> &vis){
+    //     vis[node]=1;
+    //     for(auto i:adj[node]){
+    //         if(!vis[i]){
+    //             dfs(i,adj,vis);
+    //         }else{
+    //             if(ans.find(i)!=ans.end())ans.erase(i);
+    //         }
+    //     }
+    // }
     vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
-        ans.clear();
-        vector<int> vis(n,0);
-        vector<vector<int>> adj(n);
-        for(int i=0;i<edges.size();i++){
-            adj[edges[i][0]].push_back(edges[i][1]);
-        }
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                ans.insert(i);
-                dfs(i,adj,vis);
-            }
-        }
-        vector<int> v;
-        for(auto i:ans){
-            v.push_back(i);
-        }return v;
+        vector<int> res, visited(n);
+
+        for (auto& e: edges)
+            visited[e[1]] = 1;
+
+        for (int i = 0; i < n; ++i)
+            if (visited[i] == 0)
+                res.push_back(i);
+                
+        return res;
     }
 };
