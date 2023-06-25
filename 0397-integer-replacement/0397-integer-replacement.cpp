ee2f36b3922d0a1b31d2d981a7852cab9c22b378
Dcoder10M <84701974+Dcoder10M@users.noreply.github.com>
2023-06-25 19:25:52 +0530

    Time: 3 ms (62.49%), Space: 5.9 MB (64.28%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int integerReplacement(long long n) {
+        if(n==1)return 0;
+        if(n%2==0)return 1+integerReplacement(n/2);
+        return 1+min(integerReplacement(n-1),integerReplacement(n+1));
+    }
+};
