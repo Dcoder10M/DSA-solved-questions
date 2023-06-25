79591455fc37ec62b14ec4b143a0368747ce95fa
Dcoder10M <84701974+Dcoder10M@users.noreply.github.com>
2023-06-26 02:37:30 +0530

    Time: 0 ms (100%), Space: 12.4 MB (44.63%) - LeetHub

--- 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.cpp
+++ 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.cpp
@@ -9,7 +9,7 @@ public:
         int ans=0;
         int mn=*min_element(nums.begin(),nums.end());
         int mx=*max_element(nums.begin(),nums.end());
-        ans=gcd(mx,mn);
+        ans=gcd(mn,mx);
         return ans;
     }
 };
