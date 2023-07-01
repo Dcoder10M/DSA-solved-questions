9fade9ce627041ad127c933dc23cdbc2159b3f0e
Dcoder10M <84701974+Dcoder10M@users.noreply.github.com>
2023-07-01 23:24:10 +0530

    Time: 422 ms (39.8%), Space: 14.3 MB (45.81%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    int ladderLength(string A, string B, vector<string> &C) {
+    int ladderLength(string begin, string end, vector<string>& wordList) {
         set<string> s;
-        for(auto i:C)s.insert(i);
+        for(auto i:wordList)s.insert(i);
+        s.erase(begin);
         queue<pair<string,int>> q;
-        q.push({A,1});
-        s.erase(A);
+        q.push({begin,1});
         while(!q.empty()){
             string temp=q.front().first;
             int curr=q.front().second;
             q.pop();
             for(int i=0;i<temp.size();i++){
                 char og=temp[i];
-                for(char j='a';j<='z';j++){
-                    if(temp[i]==j)continue;
-                    temp[i]=j;
+                for(char c='a';c<='z';c++){
+                    if(og==c)continue;
+                    temp[i]=c;
                     if(s.find(temp)!=s.end()){
-                        if(temp==B)return curr+1;
-                        s.erase(temp);
+                        if(temp==end)return curr+1;
                         q.push({temp,curr+1});
-                    }temp[i]=og;
-                }
+                        s.erase(temp);
+                    }
+                }temp[i]=og;
             }
         }return 0;
     }
