a8b6186963dcd9f1580a69a466e30731446bef44
Dcoder10M <84701974+Dcoder10M@users.noreply.github.com>
2023-07-02 00:31:57 +0530

    Time: 170 ms (71.93%), Space: 48.2 MB (66.18%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -7,16 +7,16 @@ public:
           for(auto it : graph[node]){
               if(vis[it]!=1){ 
                    if(dfs(it,graph,vis,pathvis,safe)==true){ 
-                         safe[it]=0; 
+                        //  safe[it]=0; 
                         return true;
                    }
               }
               else if(pathvis[it]==1){ 
-                    safe[it]=0;   
+                    // safe[it]=0;   
                     return true;
               }
           }
-         safe[node]=1;   
+        //  safe[node]=1;   
          pathvis[node]=0;
          return false;   
      }
@@ -33,10 +33,45 @@ public:
              }
         }
         for(int i=0;i<n;i++){
-            if(safe[i]==1){
+            if(!(pathvis[i]==1 && vis[i]==1)){
                 safenode.push_back(i);
             }
         }
         return safenode;
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     bool dfs(int node,vector<vector<int>> &adj,vector<int> &vis,vector<int> &path){
+//         vis[node]=1;
+//         path[node]=1;
+//         for(auto i:adj[node]){
+//             if(!vis[i]){
+//                 if(dfs(i,adj,vis,node)==true){
+//                     return true;
+//                 }
+//             }else if(path[i]==1){
+//                 return true;
+//             }
+//         }
+//         path[node]=0;
+//         return false;
+//     }
+//     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+//         int n=graph.size();
+//         vector<int> vis(n,0);
+//         vector<int> path(n,0);
+//         for(int i=0;i<n;i++){
+//             if(!vis[i]){
+//                 dfs(i,graph,vis,path);
+//             }
+//         }vector<int> ans;
+//         for(int i=0;i<n;i++){
+//             if(vis[i]==1 && path[i]==1)ans.push_back(i);
+//         }return ans;
+//     }
+// };
