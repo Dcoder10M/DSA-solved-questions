c4f5e27ff9b5d257af5496ef79a4af39b5732c4e
Dcoder10M <84701974+Dcoder10M@users.noreply.github.com>
2023-07-02 17:44:21 +0530

    Time: 555 ms (16.09%), Space: 95.5 MB (28.11%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -11,11 +11,10 @@ public:
         for(int i=0;i<=n;i++){
             dp[i][0]=1;
         }
-            for(int j=1;j<=sum;j++){
         for(int i=1;i<=n;i++){
+            for(int j=1;j<=sum;j++){
 
-                if(nums[i-1]==j)dp[i][j]=1;
-                else if(j>=nums[i-1])
+                if(j>=nums[i-1])
                 dp[i][j]=dp[i-1][j-nums[i-1]]||dp[i-1][j];
                 
                 else dp[i][j]=dp[i-1][j];
