914ee045fa12631528631767c94554fbd9199112
Dcoder10M <84701974+Dcoder10M@users.noreply.github.com>
2023-07-02 22:00:44 +0530

    Time: 0 ms (100%), Space: 7 MB (15.17%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -4,28 +4,30 @@ public:
         int n=a.size(),m=a[0].size();
         int top=0,bottom=n-1,left=0,right=m-1;
         int dir=0;
-        // int ans=0;
-        // int cnt=0;
         vector<int> ans;
         while(top<=bottom && left<=right){
-            if(dir==0){
+            // if(dir==0){
                 for(int i=left;i<=right;i++){
                     ans.push_back(a[top][i]);
                 }
                 top++;
-            }else if(dir==1){
+                if(!(top<=bottom && left<=right))break;
+            // }else if(dir==1){
                 for(int i=top;i<=bottom;i++){
                     ans.push_back(a[i][right]);
                 }right--;
-            }else if(dir==2){
+            // }else if(dir==2){
+                if(!(top<=bottom && left<=right))break;
                 for(int i=right;i>=left;i--){
                     ans.push_back(a[bottom][i]);
                 }bottom--;
-            }else{
+            // }else{
+                if(!(top<=bottom && left<=right))break;
                 for(int i=bottom;i>=top;i--){
                     ans.push_back(a[i][left]);
                 }left++;
-            }dir=(dir+1)%4;
+                if(!(top<=bottom && left<=right))break;
+            // }dir=(dir+1)%4;
         }return ans;
     }
 };
