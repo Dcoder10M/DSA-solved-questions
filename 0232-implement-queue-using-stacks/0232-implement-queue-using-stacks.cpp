279e5144377933617c4417bb100ef4a9d30cd2a3
Dcoder10M <84701974+Dcoder10M@users.noreply.github.com>
2023-07-09 00:53:28 +0530

    Time: 0 ms (100%), Space: 6.8 MB (94.01%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -6,17 +6,16 @@ public:
     }
     
     void push(int x) {
-        while(!st2.empty()){
-            st1.push(st2.top());
-            st2.pop();
-        }
         st1.push(x);
     }
     
     int pop() {
-        while(!st1.empty()){
-            st2.push(st1.top());
-            st1.pop();
+        if(st2.empty() && st1.empty())return -1;
+        if(st2.empty()){
+            while(!st1.empty()){
+                st2.push(st1.top());
+                st1.pop();
+            }
         }
         int a=st2.top();
         st2.pop();
@@ -24,9 +23,12 @@ public:
     }
     
     int peek() {
-        while(!st1.empty()){
-            st2.push(st1.top());
-            st1.pop();
+        if(st2.empty() && st1.empty())return -1;
+        if(st2.empty()){
+            while(!st1.empty()){
+                st2.push(st1.top());
+                st1.pop();
+            }
         }
         return st2.top();
     }
