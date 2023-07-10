508b19d0b65969e7a823ac93bd1e8c877c459b1b
Dcoder10M <84701974+Dcoder10M@users.noreply.github.com>
2023-07-10 17:39:26 +0530

    Time: 202 ms (69.02%), Space: 89.1 MB (21.89%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -6,27 +6,7 @@ public:
         ll ans=0;
         stack<int> s;
         int n=nums.size();
-        vector<ll> mxr(n),mxl(n),mnr(n),mnl(n);
-        for(int i=0;i<n;i++){
-            while(!s.empty() && nums[s.top()]<=nums[i]){
-                s.pop();
-            }
-            if(s.empty()){
-                mxl[i]=-1;
-            }else mxl[i]=s.top();
-            s.push(i);
-        }
-        while(!s.empty())s.pop();
-        for(int i=n-1;i>=0;i--){
-            while(!s.empty() && nums[s.top()]<nums[i]){
-                s.pop();
-            }
-            if(s.empty()){
-                mxr[i]=n;
-            }else mxr[i]=s.top();
-            s.push(i);
-        }
-        while(!s.empty())s.pop();
+        vector<ll> mnr(n),mnl(n);
         for(int i=0;i<n;i++){
             while(!s.empty() && nums[s.top()]>nums[i]){
                 s.pop();
