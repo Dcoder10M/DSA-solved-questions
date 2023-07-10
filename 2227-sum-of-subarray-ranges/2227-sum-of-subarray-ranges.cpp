ac936dabb8d5670e8dd53809d2093d56342b71bb
Dcoder10M <84701974+Dcoder10M@users.noreply.github.com>
2023-07-10 17:02:30 +0530

    Time: 20 ms (76.72%), Space: 12 MB (8.96%) - LeetHub

---
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
@@ -0,0 +1,56 @@
+typedef long long ll;
+
+class Solution {
+public:
+    long long subArrayRanges(vector<int>& nums) {
+        ll ans=0;
+        stack<int> s;
+        int n=nums.size();
+        vector<ll> mxr(n),mxl(n),mnr(n),mnl(n);
+        for(int i=0;i<n;i++){
+            while(!s.empty() && nums[s.top()]<=nums[i]){
+                s.pop();
+            }
+            if(s.empty()){
+                mxl[i]=-1;
+            }else mxl[i]=s.top();
+            s.push(i);
+        }
+        while(!s.empty())s.pop();
+        for(int i=n-1;i>=0;i--){
+            while(!s.empty() && nums[s.top()]<nums[i]){
+                s.pop();
+            }
+            if(s.empty()){
+                mxr[i]=n;
+            }else mxr[i]=s.top();
+            s.push(i);
+        }
+        while(!s.empty())s.pop();
+        for(int i=0;i<n;i++){
+            while(!s.empty() && nums[s.top()]>=nums[i]){
+                s.pop();
+            }
+            if(s.empty()){
+                mnl[i]=-1;
+            }else mnl[i]=s.top();
+            s.push(i);
+        }
+        while(!s.empty())s.pop();
+        for(int i=n-1;i>=0;i--){
+            while(!s.empty() && nums[s.top()]>nums[i]){
+                s.pop();
+            }
+            if(s.empty()){
+                mnr[i]=n;
+            }else mnr[i]=s.top();
+            s.push(i);
+        }
+        for(int i=0;i<n;i++){
+            ll temp=(i-mxl[i])*(mxr[i]-i);
+            ans+=temp*nums[i];
+            temp=(i-mnl[i])*(mnr[i]-i);
+            ans-=temp*nums[i];
+        }return ans;
+    }
+};
