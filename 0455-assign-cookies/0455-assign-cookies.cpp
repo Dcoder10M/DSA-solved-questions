b2acca9290f12269d08d59ac01cd2a90761d7984
Dcoder10M <84701974+Dcoder10M@users.noreply.github.com>
2023-07-10 19:44:15 +0530

    Time: 41 ms (18.04%), Space: 17.3 MB (95.59%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int ans=0;
+        int x=0,y=0;
+        while(x<g.size() && y<s.size()){
+            if(s[y]>=g[x]){
+                ans++;
+                x++;
+                y++;
+            }else y++;
+        }return ans;
+    }
+};
