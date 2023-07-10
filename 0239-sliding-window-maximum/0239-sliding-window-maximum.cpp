08dc5b4e1ad2a2d9c288c7ecdaf9a92cab6b6ec9
Dcoder10M <84701974+Dcoder10M@users.noreply.github.com>
2023-07-10 19:14:04 +0530

    Time: 786 ms (5.07%), Space: 212.1 MB (6.39%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int> dq;
-        int n=nums.size();
+        multiset<int> pq;
         vector<int> ans;
-        for(int i=0;i<n;i++){
-            if(!dq.empty() && dq.front()==i-k)dq.pop_front();
-            while(!dq.empty() && nums[dq.back()]<=nums[i]){
-                dq.pop_back();
+        for(int i=0;i<nums.size();i++){
+            pq.insert(nums[i]);
+            if(i>=k-1){
+                auto it=pq.end();
+                it--;
+                ans.push_back(*it);
+                pq.erase(pq.find(nums[i-k+1]));
             }
-            dq.push_back(i);
-            if(i>=k-1)ans.push_back(nums[dq.front()]);
         }return ans;
     }
 };
