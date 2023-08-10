d72b9d6d1c37482c55364806b5c64c2644b47142
Dcoder10M <84701974+Dcoder10M@users.noreply.github.com>
2023-08-10 13:38:02 +0530

    Time: 3 ms (40.7%), Space: 6.8 MB (94.29%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.cpp
+++ 0289-game-of-life/0289-game-of-life.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     void gameOfLife(vector<vector<int>>& board) {
-        vector<vector<int>> ans=board;
-        int n=ans.size();
-        int m=ans[0].size();
+        int n=board.size();
+        int m=board[0].size();
         int dx[]={-1,-1,0,1,1,1,0,-1};
         int dy[]={0,1,1,1,0,-1,-1,-1};
         for(int i=0;i<n;i++){
@@ -11,12 +10,21 @@ public:
                 int count=0;
                 for(int x=0;x<8;x++){
                     if(i+dx[x]>=0 && i+dx[x]<n && j+dy[x]>=0 && j+dy[x]<m)
-                    count+=board[i+dx[x]][j+dy[x]];
+                    count+=(board[i+dx[x]][j+dy[x]])%10;
                 }
-                if(count<2)ans[i][j]=0;
-                else if(count==3 && board[i][j]==0)ans[i][j]=1;
-                else if(count>3)ans[i][j]=0;
+                board[i][j]+=count*10;
             }
-        }board=ans;
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int count=board[i][j]/10;
+                if(count<2)board[i][j]=0;
+                else if(count==3 && board[i][j]%10==0)board[i][j]=1;
+                else if(count>3)board[i][j]=0;
+                else{
+                    board[i][j]=board[i][j]%10;
+                }
+            }
+        }
     }
 };
