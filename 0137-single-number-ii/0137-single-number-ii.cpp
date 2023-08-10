58cb56a47ce60e283ed1b776ba438524ccac8e44
Dcoder10M <84701974+Dcoder10M@users.noreply.github.com>
2023-08-10 15:30:34 +0530

    Time: 15 ms (14.39%), Space: 9.5 MB (40.87%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    long long singleNumber(vector<int>& nums) {
+        int op=0;
+        for(auto i:nums){
+            if(i==INT_MIN)op++;
+        }   if(op==1)return INT_MIN;
+        op=0;
+        for(auto i:nums){
+            if(i==INT_MAX)op++;
+        }   if(op==1)return INT_MAX;
+        map<long long,long long> m;
+        for(long long i=0;i<31;i++){
+            long long temp=1<<i;
+            for(auto j:nums){
+                j=abs(j);
+                if((temp&j)!=0)
+                m[i]++;
+            }
+        }
+        long long ans=0;
+        for(long long i=0;i<31;i++){
+            if(m[i]%3==1){
+                ans+=1<<i;
+            }
+        }
+        long long curr=0;
+        for(long long i=0;i<nums.size();i++){
+            if(nums[i]==ans)curr++;
+        }
+        if(curr==1)
+        return ans;
+        return -1*ans;    
+    }
+};
