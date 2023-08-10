c1610f118bb1711f4cc2dd8e486420993caa526d
Dcoder10M <84701974+Dcoder10M@users.noreply.github.com>
2023-08-10 13:39:33 +0530

    Time: 0 ms (100%), Space: 6.8 MB (69.39%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.cpp
+++ 0289-game-of-life/0289-game-of-life.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
     void gameOfLife(vector<vector<int>>& board) {
-        int n=board.size();
-        int m=board[0].size();
-        int dx[]={-1,-1,0,1,1,1,0,-1};
-        int dy[]={0,1,1,1,0,-1,-1,-1};
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                int count=0;
-                for(int x=0;x<8;x++){
-                    if(i+dx[x]>=0 && i+dx[x]<n && j+dy[x]>=0 && j+dy[x]<m)
-                    count+=(board[i+dx[x]][j+dy[x]])%10;
-                }
-                board[i][j]+=count*10;
-            }
-        }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                int count=board[i][j]/10;
-                if(count<2)board[i][j]=0;
-                else if(count==3 && board[i][j]%10==0)board[i][j]=1;
-                else if(count>3)board[i][j]=0;
-                else{
-                    board[i][j]=board[i][j]%10;
-                }
-            }
+    int m = board.size(), n = m ? board[0].size() : 0;
+    for (int i=0; i<m; ++i) {
+        for (int j=0; j<n; ++j) {
+            int count = 0;
+            for (int I=max(i-1, 0); I<min(i+2, m); ++I)
+                for (int J=max(j-1, 0); J<min(j+2, n); ++J)
+                    count += board[I][J] & 1;
+            if (count == 3 || count - board[i][j] == 3)
+                board[i][j] |= 2;
         }
     }
+    for (int i=0; i<m; ++i)
+        for (int j=0; j<n; ++j)
+            board[i][j] >>= 1;
+}
 };
