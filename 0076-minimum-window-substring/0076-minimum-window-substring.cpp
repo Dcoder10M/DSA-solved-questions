bba383f6dff37cf784a5498768a3aed8960699cd
Dcoder10M <84701974+Dcoder10M@users.noreply.github.com>
2023-08-10 13:21:50 +0530

    Time: 7 ms (93.82%), Space: 8 MB (36.03%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,42 +1,26 @@
 class Solution {
-public:
-    //check function to check if "t" is a substring in curr_s substring 
-    bool check(map<char,int> &map_s,map<char,int> &map_t){
-        for(auto &itr: map_t){
-            if(map_s[itr.first] < itr.second) return false;
-        }
-        return true;
-    }
+    public:
     string minWindow(string s, string t) {
-        if(s.size() < t.size() ){
-            return "";
-        }
-        if(s == t) return s;
-        int resulti = 0;
-        int anslen  = INT_MAX;
-        int i=0;
-        int j=0;
-        int n = s.size();
-        map<char,int> map_t;
-        map<char,int> map_s;
-        for(auto &itr:t){ //feed string 't' into map_t
-            map_t[itr]++;
-        }
-        //sliding window
-        while(j<n){
-            map_s[s[j]]++;
-            while(check(map_s,map_t)){
-                if(j-i+1 <= anslen){
-                    anslen = j-i+1;
-                    resulti = i;
-                }
-                map_s[s[i]]--;
-                if(map_s[s[i]]==0) map_s.erase(s[i]);
-                i++;
+      
+      vector<int> v(128,0);
+        int count =t.size(),minl=INT_MAX,inii=0,j=0;
+      for(auto i: t)
+      v[i]++;
+
+       for(int i=0;i<s.size();i++){
+            if(v[s[i]]>0) count--;
+            v[s[i]]--;
+
+            while(count==0){
+                    if(minl>i-j+1){
+                        minl=i-j+1;
+                        inii=j;
+                    }
+                    v[s[j]]++;
+                    if(v[s[j]]>0) count++;
+                    j++;
             }
-            j++;
-        }
-        if(anslen==INT_MAX) return "";
-        return s.substr(resulti,anslen);
+       }
+       return minl==INT_MAX? "":s.substr(inii,minl);
     }
 };
