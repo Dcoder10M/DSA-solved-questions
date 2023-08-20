b22a7c2963eb1a227141f0a0f93f5c6270682cfa
Dcoder10M <84701974+Dcoder10M@users.noreply.github.com>
2023-08-20 18:20:55 +0530

    Time: 5 ms (82.07%), Space: 12.1 MB (15.34%) - LeetHub

--- 2347-count-nodes-equal-to-average-of-subtree/2347-count-nodes-equal-to-average-of-subtree.cpp
+++ 2347-count-nodes-equal-to-average-of-subtree/2347-count-nodes-equal-to-average-of-subtree.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    int s(TreeNode* root,int &v){
-        if(root==NULL)return 0;
-        v++;
-        return root->val + s(root->right,v) + s(root->left,v);
+    int x = 0;
+    pair<int,int> solve(TreeNode* root){
+        if(root==NULL)return {0,0};
+        pair<int,int> p1 = solve(root->left);
+        pair<int,int> p2 = solve(root->right);
+        int x1 = p1.first+p2.first+root->val;
+        int y = p1.second+p2.second+1;
+        if(root->val == (x1/y)){
+            x++;
+        }
+        return {x1,y};
     }
-    int ct=0;
     int averageOfSubtree(TreeNode* root) {
-        if(root==NULL)return 0;
-        int v=0;
-        int x = s(root,v);
-        if(root->val == (x/v))ct++;
-        averageOfSubtree(root->right);
-        averageOfSubtree(root->left);
-        return ct;
+        x = 0;
+        solve(root);
+        return x;
     }
 };
