4f2f2a8fd45a8bd98b5c6b00bddc4ed529f2fbc4
Dcoder10M <84701974+Dcoder10M@users.noreply.github.com>
2023-08-21 23:49:38 +0530

    Time: 3312 ms (5.02%), Space: 0B (100%) - LeetHub

--- 2127-employees-whose-manager-left-the-company/2127-employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/2127-employees-whose-manager-left-the-company.sql
@@ -3,6 +3,6 @@ SELECT employee_id
 FROM Employees
 WHERE salary < 30000 
 AND manager_id NOT IN (
-    SELECT DISTINCT(employee_id) FROM Employees
+    SELECT (employee_id) FROM Employees
 )
 ORDER BY employee_id;
