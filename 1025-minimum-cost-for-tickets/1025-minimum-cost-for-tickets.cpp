06d1d63a6d4fbd1996200907bb7c0265835dffb2
Dcoder10M <84701974+Dcoder10M@users.noreply.github.com>
2023-08-27 18:57:59 +0530

    Time: 0 ms (100%), Space: 9.7 MB (50.58%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
-    int dp[400][500];
-    int solve(int ind,vector<int>& days,vector<int>& costs,int upto){
-        if(ind==days.size())return 0;
-        if(dp[ind][upto]!=-1)return dp[ind][upto];
-        int ans=INT_MAX;
-        ans=min(
-            {costs[0]+solve(ind+1,days,costs,days[ind]),
-            costs[1]+solve(ind+1,days,costs,days[ind]+6),
-            costs[2]+solve(ind+1,days,costs,days[ind]+29)
-            });
-        if(upto>=days[ind]){
-            ans=min(ans,solve(ind+1,days,costs,upto));
-        }return dp[ind][upto]=ans;
+    int mincostTickets(vector<int>& days, vector<int>& costs, int cost = 0) {
+    queue<pair<int, int>> last7, last30;
+
+    for (auto d : days) {
+        while (!last7.empty() && last7.front().first + 7 <= d) last7.pop();
+        while (!last30.empty() && last30.front().first + 30 <= d) last30.pop();
+        
+        last7.push({ d, cost + costs[1] });
+        last30.push({ d, cost + costs[2] });
+        
+        cost = min({ cost + costs[0], last7.front().second, last30.front().second });
     }
-    int mincostTickets(vector<int>& days, vector<int>& costs) {
-        memset(dp,-1,sizeof(dp));
-        return solve(0,days,costs,0);
+    return cost;
     }
 };
