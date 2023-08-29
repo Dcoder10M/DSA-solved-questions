3d8fa0bc21cc0c12d9d72aff36f796ddb87e9795
Dcoder10M <84701974+Dcoder10M@users.noreply.github.com>
2023-08-30 05:02:25 +0530

    Time: 14 ms (99.35%), Space: 10.6 MB (86.38%) - LeetHub

--- 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.cpp
+++ 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.cpp
@@ -1,26 +1,14 @@
 class Solution {
 public:
-    int bestClosingTime(string s) {
-        int n=s.size();
-        vector<int> suf(n+1,0);
-        int curr=0;
-        for(int i=n-1;i>=0;i--){
-            if(s[i]=='Y')curr++;
-            suf[i]=curr;
+    int bestClosingTime(string customers) {
+        int max_score = 0, score = 0, right_time = -1;
+        for(int i = 0; i < customers.size(); ++i) {
+            score += (customers[i] == 'Y') ? 1 : -1;
+            if(score > max_score) {
+                max_score = score;
+                right_time = i;
+            }
         }
-        curr=0;
-        int ans=INT_MAX;
-        int ret=0;
-        for(int i=0;i<n;i++){
-            if(ans>suf[i]+curr){
-                ans=suf[i]+curr;
-                ret=i;
-            }   
-            if(s[i]=='N')curr++;
-        }
-        if(ans>curr){
-            return n;
-        }
-        return ret;
+        return right_time + 1;
     }
 };
