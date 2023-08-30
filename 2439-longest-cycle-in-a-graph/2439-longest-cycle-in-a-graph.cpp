f43c381bfa8533f5ebcf700107b7123ab6ae3e70
Dcoder10M <84701974+Dcoder10M@users.noreply.github.com>
2023-08-31 02:39:43 +0530

    Time: 443 ms (32.43%), Space: 222.1 MB (15.38%) - LeetHub

--- 2439-longest-cycle-in-a-graph/2439-longest-cycle-in-a-graph.cpp
+++ 2439-longest-cycle-in-a-graph/2439-longest-cycle-in-a-graph.cpp
@@ -1,53 +1,26 @@
 class Solution {
-
-    void dfs(int node, unordered_map<int,int>&mp, int cnt, vector<int>adj[], vector<int>&vis, vector<int>&pathVis, int&ans,int st)
-    {
-        vis[node]=1;
-
-        pathVis[node]=st;
-      mp[node]= cnt;
-    cnt++;
-        for(auto it: adj[node])
-        {
-            if(!vis[it]){
-                //cnt++;
-                dfs(it, mp, cnt, adj, vis, pathVis, ans,st);
-            }
-            else if(pathVis[it]==st){
-                
-                //cout<<it<<endl;
-                //cout<<mp[it]<<" "<<endl;
-                ans= max(ans, cnt-mp[it] );
-               
-            }
-        }
-       // pathVis[node]=0;
-    }
-
 public:
+    int ans=-1;
+    void dfs(int node,vector<int>& edges,unordered_map<int,int> &m,vector<int> &vis){
+        vis[node]=1;
+        int child=edges[node];
+        if(child==-1)return;
+        if(!vis[child]){
+            m[child]=1+m[node];
+            dfs(child,edges,m,vis);
+        }else if(m.find(child)!=m.end()){
+            ans=max(ans,m[node]-m[child]+1);
+        }
+    }
     int longestCycle(vector<int>& edges) {
-        int n= edges.size();
-        vector<int> adj[n];
-
-        for(int i=0; i<edges.size(); i++)
-        {
-            int u= i, v= edges[i];
-            if(v==-1) continue;
-            adj[u].push_back(v);
-        }
-
-        vector<int> vis(n+1, 0);
-        vector<int> pathVis(n+1, 0);
-        int ans=-1;
-        for(int i=0; i<n; i++)
-        {
-            unordered_map<int,int> mp;
-            int cnt=0;
+        int n=edges.size();
+        vector<int> vis(n,0);
+        for(int i=0;i<n;i++){
             if(!vis[i]){
-                int st=i;
-                dfs(i, mp, cnt, adj, vis, pathVis, ans,st);
+                unordered_map<int,int> m;
+                m[i]=1;
+                dfs(i,edges,m,vis);
             }
-        }
-        return ans;
+        }return ans;
     }
 };
