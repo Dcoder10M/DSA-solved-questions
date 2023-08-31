dcf221af4436655806f5e009e6c0954fdb52c674
Dcoder10M <84701974+Dcoder10M@users.noreply.github.com>
2023-09-01 01:06:35 +0530

    Time: 29 ms (63.36%), Space: 14.1 MB (80.3%) - LeetHub

--- 1451-minimum-number-of-taps-to-open-to-water-a-garden/1451-minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1451-minimum-number-of-taps-to-open-to-water-a-garden/1451-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -3,7 +3,7 @@ public:
     int minTaps(int n, vector<int>& ranges) {
         int min=0,max=0,open=0,index=0;
         while(max<n){
-            for(int i=index;i<ranges.size();i++){
+            for(int i=index;i<n+1;i++){
                 if(i-ranges[i]<=min && i+ranges[i]>max){
                     max=i+ranges[i];
                     index=i;
