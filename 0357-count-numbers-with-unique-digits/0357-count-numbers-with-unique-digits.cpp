dad44bb01983c99aa531045103725e135a0fd380
Dcoder10M <84701974+Dcoder10M@users.noreply.github.com>
2023-09-28 17:20:55 +0530

    Time: 5 ms (10.28%), Space: 9 MB (6.26%) - LeetHub

--- 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.cpp
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
+    int dp[10][1<<11][3][3][3];
     int solve(int ind,int mask,int tight,int lz,int repeated,string &s){
         if(ind==s.size()){
             if(repeated)return 1;
             return 0;
         }
+        if(dp[ind][mask][tight][lz][repeated]!=-1)return dp[ind][mask][tight][lz][repeated]; 
         int limit=9;
         int ans=0;
         if(tight)limit=s[ind]-'0';
@@ -18,7 +20,7 @@ public:
             }else{
                 ans+=solve(ind+1,mask|(1<<i),(tight&&(i==limit)),0,repeated,s);
             }
-        }return ans;
+        }return dp[ind][mask][tight][lz][repeated]=ans;
     }
     int countNumbersWithUniqueDigits(int n) {
         string s="";
@@ -26,6 +28,7 @@ public:
         while(n--){
             s.push_back('9');
         }
+        memset(dp,-1,sizeof(dp));
         // cout<<s<<endl;
         return temp-solve(0,0,1,1,0,s);
     }
