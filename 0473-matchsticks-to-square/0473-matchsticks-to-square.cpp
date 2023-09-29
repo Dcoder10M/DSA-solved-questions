65efd744b65e8bb215d698677bc7e9991a4ae47d
Dcoder10M <84701974+Dcoder10M@users.noreply.github.com>
2023-09-29 21:51:46 +0530

    Time: 11 ms (94.27%), Space: 10.4 MB (34.65%) - LeetHub

--- 0473-matchsticks-to-square/0473-matchsticks-to-square.cpp
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.cpp
@@ -1,71 +1,33 @@
 class Solution {
 public:
-    
-    // declare a sides array of length 4
-    
-    vector<int> sides = {0, 0, 0, 0};
-    
-    bool helper(vector<int>& matchsticks, int i, int n, int target)
-    {
-        // base case
-        
-        if(i == n)
-        {
-            return true;
+    bool solve(vector<int> &a,int ind,vector<int> &side){
+        if(ind==a.size()){
+            return side[0]==side[1] && side[1]==side[2] && side[2]==side[3];
         }
-        
-        // fill the sides array, with every possible combination
-        
-        for(int k = 0; k < 4; k++)
-        {
-            if(sides[k] + matchsticks[i] <= target)
-            {
-                // add the matchsticks's length to sides
-                
-                sides[k] += matchsticks[i];
-                
-                // recusivilly call the helper
-                
-                if(helper(matchsticks, i + 1, n, target))
-                    return true;
-                
-                // if added length doesn't lead to result then backtrack
-                
-                sides[k] -= matchsticks[i];
+        for(int i=0;i<4;i++){
+            if(side[i]<a[ind])continue;
+            int j=i-1;
+            bool ans=false;
+            while(j>=0){
+                if(side[i]==side[j]){
+                    ans=true;
+                    break;
+                }
+                j--;
             }
-        }
-        
-        return false;
+            if(ans)continue;
+            side[i]-=a[ind];
+            if(solve(a,ind+1,side))return true;
+            side[i]+=a[ind];
+        }return false;
     }
-    
-    bool makesquare(vector<int>& matchsticks) {
-        
-        int n = matchsticks.size();
-        
-        // find total sum of length of matchsticks
-        
-        int sum = 0;
-        
-        for(int i = 0; i < n; i++)
-        {
-            sum += matchsticks[i];
-        }
-        
-        // if sum is not divisible by 4, then can't be divided into 4 equal set
-        
-        if(sum % 4)
-            return false;
-        
-        // size of each equal set
-        
-        int target = sum / 4;
-        
-        // sort the matchsticks into descending order, to avoid TLE
-        
-        sort(matchsticks.begin(), matchsticks.end(), greater<int> ());
-        
-        // call the helper function
-        
-        return helper(matchsticks, 0, n, target);
+    bool makesquare(vector<int>& a) {
+        int sum=0;
+        for(auto i:a)sum+=i;
+        if(sum%4!=0)return false;
+        int peri=sum/4;
+        sort(a.rbegin(),a.rend());
+        vector<int> side={peri,peri,peri,peri};
+        return solve(a,0,side);
     }
 };
