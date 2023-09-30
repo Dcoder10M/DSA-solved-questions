0837618a7444d306eaf12c61517f5728a150bf2c
Dcoder10M <84701974+Dcoder10M@users.noreply.github.com>
2023-09-30 17:29:07 +0530

    Time: 47 ms (79.56%), Space: 9 MB (72.54%) - LeetHub

--- 0993-tallest-billboard/0993-tallest-billboard.cpp
+++ 0993-tallest-billboard/0993-tallest-billboard.cpp
@@ -1,21 +1,22 @@
 class Solution {
-public:
-    int helper(int index,int sum,vector<int>&rods,vector<vector<int>>&dp){
-        int n= rods.size();
-        if(index>=n){
-            if(sum==5000) return 0;
-            return INT_MIN;
+private:
+    int solver(vector<int>& rods, int i, int diff, int dp[][10001]){
+        if(i == rods.size()){
+            if(diff == 0) return 0;
+            return -6000;
         }
-        if(dp[index][sum]!=INT_MIN) return dp[index][sum];
-        int option1= helper(index+1,sum,rods,dp);
-        int option2= helper(index+1, sum-rods[index],rods,dp);
-        int option3= rods[index]+helper(index+1,sum+rods[index],rods,dp);
-        return dp[index][sum]=max({option1,option2,option3});
+        if(dp[i][diff+5000] != -1) return dp[i][diff+5000];
+
+        int option1 = solver(rods,i+1,diff,dp);
+        int option2 = rods[i] + solver(rods,i+1,diff+rods[i],dp);
+        int option3 = rods[i] + solver(rods,i+1,diff-rods[i],dp);
+
+        return dp[i][diff+5000] = max(option1,max(option2,option3));
     }
+public:
     int tallestBillboard(vector<int>& rods) {
-        int n= rods.size();
-        vector<vector<int>>dp(n,vector<int>(10001,INT_MIN));
-        return helper(0,5000,rods,dp);
-        
+        int dp[21][10001];
+        memset(dp,-1,sizeof(dp));
+        return solver(rods,0,0,dp)/2;
     }
 };
