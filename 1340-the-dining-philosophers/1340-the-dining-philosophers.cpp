92e1274012d3c1296adcca6e1988a4a9469eba2a
Dcoder10M <84701974+Dcoder10M@users.noreply.github.com>
2023-10-06 16:40:53 +0530

    Time: 55 ms (15.37%), Space: 11.6 MB (9.73%) - LeetHub

--- 1340-the-dining-philosophers/1340-the-dining-philosophers.cpp
+++ 1340-the-dining-philosophers/1340-the-dining-philosophers.cpp
@@ -3,15 +3,29 @@ public:
     DiningPhilosophers() {
         
     }
-    mutex mtx;
+    mutex mtx[5];
     void wantsToEat(int philosopher,
                     function<void()> pickLeftFork,
                     function<void()> pickRightFork,
                     function<void()> eat,
                     function<void()> putLeftFork,
                     function<void()> putRightFork) {
-                        mtx.lock();
-        pickLeftFork(); pickRightFork(); eat(); putLeftFork(); putRightFork();
-        mtx.unlock();
+        int left = philosopher;
+        int right = (philosopher + 1) % 5;
+        
+        if(philosopher % 2 == 0){
+            mtx[right].lock(); // lock right before left
+            mtx[left].lock();
+            pickLeftFork(); pickRightFork();
+        }
+		else{
+            mtx[left].lock(); // lock left before right
+            mtx[right].lock();
+            pickLeftFork(); pickRightFork();
+        }
+        
+        eat(); putRightFork(); putLeftFork();
+        mtx[left].unlock();
+        mtx[right].unlock();
     }
 };
