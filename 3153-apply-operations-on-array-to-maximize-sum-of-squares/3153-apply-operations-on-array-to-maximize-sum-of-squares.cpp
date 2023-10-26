4e32777fe1ee02b1f019f54d7522a3184f43176c
Dcoder10M <84701974+Dcoder10M@users.noreply.github.com>
2023-10-26 23:45:46 +0530

    Time: 233 ms (54.66%), Space: 86.4 MB (33.46%) - LeetHub

--- 3153-apply-operations-on-array-to-maximize-sum-of-squares/3153-apply-operations-on-array-to-maximize-sum-of-squares.cpp
+++ 3153-apply-operations-on-array-to-maximize-sum-of-squares/3153-apply-operations-on-array-to-maximize-sum-of-squares.cpp
@@ -4,22 +4,20 @@ public:
     int maxSum(vector<int>& nums, int k) {
         long long ans=0;
         vector<int> bits(31,0);
-        for(int i=0;i<nums.size();i++){
-            for(int j=0;j<=30;j++){
-                if(nums[i]&(1<<j)){
-                    bits[j]++;
-                }
+        for(auto num:nums){
+            for(int i=30;i>=0;i--){
+                if(((1<<i)&num)!=0)bits[i]++;
             }
         }
         while(k--){
             long long temp=0;
-            for(int i=0;i<=30;i++){
+            for(int i=30;i>=0;i--){
                 if(bits[i]){
-                    temp=temp|(1<<i);
+                    temp=(temp+(1<<i))%mod;
                     bits[i]--;
                 }
-            }ans=(ans+(temp*temp)%mod)%mod;
-        }
-        return ans;
+            }
+            ans=(ans+(temp*temp)%mod)%mod;
+        }return ans;
     }
 };
