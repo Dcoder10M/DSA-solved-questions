011582ec7f00d2f708298fd20d36278d69f36aea
Dcoder10M <84701974+Dcoder10M@users.noreply.github.com>
2024-06-12 16:30:00 +0530

    Time: 101 ms (43.48%), Space: 45.3 MB (77.57%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cs
+++ 0075-sort-colors/0075-sort-colors.cs
@@ -1,19 +1,23 @@
 public class Solution {
     public void SortColors(int[] nums) {
-        int low=0,high=nums.Length-1,mid=0;
-        while(low<high && mid<=high){
-            if(nums[mid]==0){
-                nums[mid]=nums[low];
-                nums[low++]=0;
-            }
-            else if(nums[mid]==1){
+        int low = 0, mid = 0, high = nums.Length - 1;
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                Swap(nums, low, mid);
+                low++;
                 mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                Swap(nums, mid, high);
+                high--;
             }
-            else{
-                nums[mid]=nums[high];
-                nums[high--]=2;
-            }
-            if(low>mid)mid=low;
         }
+    }
+
+    private void Swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
