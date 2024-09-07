ab2173eb4958c5db802d5c14473fdef74cb73d28
Dcoder10M <84701974+Dcoder10M@users.noreply.github.com>
2024-09-07 18:12:22 +0530

    Time: 15 ms (67.88%), Space: 17.2 MB (85.05%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -3,27 +3,30 @@ public:
     vector<int> findOrder(int A, vector<vector<int>>& pre) {
         vector<int> ans;
         vector<vector<int>> adj(A);
-        vector<int> ind(A,0);
-        queue<int> q;
-        for(auto i:pre){
-            adj[i[1]].push_back(i[0]);
-            ind[i[0]]++;
+        vector<int> dependent(A,0);
+        for(int i=0;i<pre.size();i++){
+            adj[pre[i][1]].push_back(pre[i][0]);
+            dependent[pre[i][0]]++;
         }
+        queue<int> q;
         for(int i=0;i<A;i++){
-            if(ind[i]==0){
+            if(dependent[i]==0){
                 q.push(i);
             }
         }
         while(!q.empty()){
             int node=q.front();
-            q.pop();
             ans.push_back(node);
+            q.pop();
             for(auto i:adj[node]){
-                ind[i]--;
-                if(ind[i]==0)q.push(i);
+                dependent[i]--;
+                if(dependent[i]==0){
+                    q.push(i);
+                }
             }
         }
-        if(ans.size()!=A)return {};
+        if(ans.size()==A)
         return ans;
+        return {};
     }
 };
